2a4550fd8c67211b5a21da3a623aa30b58e60789
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-03-26 11:45:24 +0100

    Created view to Read chosen Incomes or Outcomes and created tests with Fixture template and fixture that will be updated to be always up-to-date to test default return.
    Removed Fixture from git to not update it in git every month

--- finance/tests.py
+++ finance/tests.py
@@ -1,9 +1,82 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
 from django.db.models import PROTECT, CASCADE, SET_NULL
-from .models import Company, CashTag, Currency, OneInOut
+import datetime
+from .models import Company, CashTag, Currency, OneIO
+from .views import ReadIO
 
 
+
+# ---------- Views ----------
+class ReadIOView(TestCase):
+    fixtures = ['ReadIO.json']
+    this_month = datetime.date.today().replace(day=1)
+    last_month = this_month - datetime.timedelta(days=1)
+
+    @classmethod
+    def setUpClass(cls):
+
+        fixture_path = 'finance/fixtures/ReadIO.json'
+        template_fixture = 'finance/fixtures/ReadIO_template.json'
+
+        Currency.objects.create()
+        User.objects.create()
+
+        with open(template_fixture, 'r') as f:
+            f_data = f.read()
+            f_data = f_data.replace("__CURRENT_MONTH__", str(ReadIOView.this_month))
+            f_data = f_data.replace("__LAST_MONTH__", str(ReadIOView.last_month))
+        with open(fixture_path, 'w') as f:
+            f.write(f_data)
+        super().setUpClass()
+
+    def setUp(self):
+        self.r = RequestFactory()
+        self.this = ReadIOView.this_month
+        self.last = ReadIOView.last_month
+
+    def test_this_month_outcome(self):
+        r = self.r.get('/')
+        view = ReadIO()
+        view.request = r
+        qs = view.get_queryset()
+        self.assertEqual(50, qs[0].value)
+
+    def test_this_month_income(self):
+        r = self.r.get('/', {
+            'chosen_year': self.this.year,
+            'chosen_month': self.this.month,
+            'is_outcome': False
+        })
+        view = ReadIO()
+        view.request = r
+        qs = view.get_queryset()
+        self.assertEqual(100, qs[0].value)
+
+    def test_last_month_outcome(self):
+        r = self.r.get('/', {
+            'chosen_year': self.last.year,
+            'chosen_month': self.last.month,
+            'is_outcome': True
+        })
+        view = ReadIO()
+        view.request = r
+        qs = view.get_queryset()
+        self.assertEqual(90, qs[0].value)
+
+    def test_last_month_income(self):
+        r = self.r.get('/', {
+            'chosen_year': self.last.year,
+            'chosen_month': self.last.month,
+            'is_outcome': False
+        })
+        view = ReadIO()
+        view.request = r
+        qs = view.get_queryset()
+        self.assertEqual(200, qs[0].value)
+
+
+# ---------- MODELS ----------
 class TestCompany(TestCase):
     def test_company_max_len(self):
         field = Company._meta.get_field('company').max_length
@@ -32,39 +105,39 @@ class TestCurrency(TestCase):
 
 class TestOneInOut(TestCase):
     def test_is_outcome(self):
-        field = OneInOut._meta.get_field('is_outcome').default
+        field = OneIO._meta.get_field('is_outcome').default
         self.assertTrue(field)
 
     def test_owner(self):
-        field = OneInOut._meta.get_field('owner')
+        field = OneIO._meta.get_field('owner')
         to_class = field.related_model
         field_on_delete = field.remote_field.on_delete
         self.assertTrue(issubclass(User, to_class))
         self.assertEqual(CASCADE, field_on_delete)
 
     def test_title(self):
-        field = OneInOut._meta.get_field('title').max_length
+        field = OneIO._meta.get_field('title').max_length
         self.assertEqual(50, field)
 
     def test_value(self):
-        field_max = OneInOut._meta.get_field('value').max_digits
-        field_places = OneInOut._meta.get_field('value').decimal_places
+        field_max = OneIO._meta.get_field('value').max_digits
+        field_places = OneIO._meta.get_field('value').decimal_places
         self.assertEqual(17, field_max)
         self.assertEqual(2, field_places)
 
     def test_currency(self):
-        field = OneInOut._meta.get_field('currency')
+        field = OneIO._meta.get_field('currency')
         to_class = field.related_model
         field_on_delete = field.remote_field.on_delete
         self.assertTrue(issubclass(Currency, to_class))
         self.assertEqual(PROTECT, field_on_delete)
 
     def test_future(self):
-        field = OneInOut._meta.get_field('future').default
+        field = OneIO._meta.get_field('future').default
         self.assertTrue(field)
 
     def test_cash_tag(self):
-        field = OneInOut._meta.get_field('cash_tag')
+        field = OneIO._meta.get_field('cash_tag')
         to_class = field.related_model
         field_on_delete = field.remote_field.on_delete
         field_null = field.null
@@ -73,7 +146,7 @@ class TestOneInOut(TestCase):
         self.assertTrue(field_null)
 
     def test_company(self):
-        field = OneInOut._meta.get_field('company')
+        field = OneIO._meta.get_field('company')
         to_class = field.related_model
         field_on_delete = field.remote_field.on_delete
         field_null = field.null
@@ -82,15 +155,15 @@ class TestOneInOut(TestCase):
         self.assertTrue(field_null)
 
     def test_notes(self):
-        field = OneInOut._meta.get_field('notes').blank
+        field = OneIO._meta.get_field('notes').blank
         self.assertTrue(field)
 
     def test_creation_date(self):
-        field = OneInOut._meta.get_field('creation_date').auto_now_add
+        field = OneIO._meta.get_field('creation_date').auto_now_add
         self.assertTrue(field)
 
     def test_last_modification(self):
-        field = OneInOut._meta.get_field('last_modification').auto_now
+        field = OneIO._meta.get_field('last_modification').auto_now
         self.assertTrue(field)
 
     def test_ordering(self):
@@ -101,8 +174,8 @@ class TestOneInOut(TestCase):
         dates = ('1970-12-1', '1970-12-2', '1970-12-2', '1970-12-3')
 
         for v, d in zip(vals, dates):
-            OneInOut.objects.create(owner=o, value=v, currency=c, date=d)
+            OneIO.objects.create(owner=o, value=v, currency=c, date=d)
 
-        res = [obj.value for obj in OneInOut.objects.all()]
+        res = [obj.value for obj in OneIO.objects.all()]
         self.assertEqual([10, 30, 20, 100], res)
 
--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils import timezone
+from .models import OneIO
+"""
+We need a view for to display all: 
+(by default display this month)
+ outcomes 1-off
+ outcomes recurring
+ incomes 1-off
+ outcomes recurring
+"""
 
-# Create your views here.
+
+class ReadIO(ListView):
+    model = OneIO
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        chosen_year = self.request.GET.get('chosen_year')
+        chosen_month = self.request.GET.get('chosen_month')
+        chosen_is_outcome = self.request.GET.get('is_outcome')
+        if chosen_month and chosen_month:
+            return queryset.filter(date__year=chosen_year, date__month=chosen_month, is_outcome=chosen_is_outcome)
+
+        year = timezone.now().year
+        month = timezone.now().month
+        return queryset.filter(date__year=year, date__month=month, is_outcome=True)
+
+
