7efa696baeb7f3d80f59cdfd578ad1ae8d77b26f
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-03-28 12:29:21 +0100

    Added option to update model

--- finance/forms.py
+++ finance/forms.py
@@ -1,11 +1,22 @@
-from django.forms import ModelForm, ValidationError
-from .models import OneIO
+from django.forms import ModelForm, ValidationError, DateInput
+from django.contrib.auth.models import User
+from .models import OneIO, Currency
+
 
 
 class UpdateIOForm(ModelForm):
     class Meta:
         model = OneIO
         fields = ['title', 'value', 'date', 'future', 'cash_tag', 'company', 'notes']
+        widgets = {
+            'date': DateInput(attrs={'type': 'date'})  # Use DateInput widget for date field
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cash_tag'].required = False
+        self.fields['company'].required = False
+        self.fields['notes'].required = False
 
     def clean_value(self):
         value = self.cleaned_data.get('value')
@@ -19,3 +30,9 @@ class UpdateIOForm(ModelForm):
             raise ValidationError('Title has to be between 1 and 50 charts')
         return title
 
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.owner = User.objects.get(pk=1)
+        instance.currency = Currency.objects.get(pk=1)
+        instance.save()
+        return instance
