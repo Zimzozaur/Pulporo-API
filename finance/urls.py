13647706692e674ee5926d1e917b453ca34d9c19
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-03-28 12:28:39 +0100

    Created link for DeleteIO and remove redundant after merge of views

--- finance/urls.py
+++ finance/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ListIOs, DetailIO, UpdateIO
+from .views import ListIOs, DetailIO, CreateIO, DeleteIO
 
 
 urlpatterns = [
     path('', ListIOs.as_view(), name='list-io'),
-    path('detail/<int:pk>/', DetailIO.as_view(), name='detail-io'),
-    path('update/<int:pk>/', UpdateIO.as_view(), name='update-io'),
+    path('create/', CreateIO.as_view(), name='create-io'),
+    path('update/<int:pk>/', DetailIO.as_view(), name='detail-io'),
+    path('delete/<int:pk>', DeleteIO.as_view(), name='delete-io')
 ]
+
+
