1b2c9cf86020ed4f8fbee473468320e89e7d0a45
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-03-29 15:55:31 +0100

    Added DataForm JS library to make tables flexible and interactive

---
+++ static/init_datatable.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let table = new DataTable('#TableIO', {
+        paging: true,
+        order: false,
+        searching: true,
+        colReorder: true,
+        autoWidth: false,
+    });
+});
