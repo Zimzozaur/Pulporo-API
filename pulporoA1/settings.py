3e1f2693b08a382f8a93125ce45b48ed57528313
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-02 12:26:25 +0200

    Improvement: Added static to settings.py

--- pulporoA1/settings.py
+++ pulporoA1/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'pulporoA1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
