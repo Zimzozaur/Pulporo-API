96612e5a3a3e1ac8688d509190d92997e319a866
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-04 16:36:13 +0200

    Refactor: Change name to a be same as path

--- finance/urls.py
+++ finance/urls.py
@@ -3,7 +3,7 @@ from .views import ListIOs, DetailIO, CreateIO, DeleteIO
 
 
 urlpatterns = [
-    path('ledger/', ListIOs.as_view(), name='list-io'),
+    path('ledger/', ListIOs.as_view(), name='ledger'),
     path('create/', CreateIO.as_view(), name='create-io'),
     path('update/<int:pk>/', DetailIO.as_view(), name='detail-io'),
     path('delete/<int:pk>', DeleteIO.as_view(), name='delete-io')
