2f2c59b3e6d8c233a5349baa83fb7f339034b490
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-05 08:51:59 +0200

    Changed app name inside files

--- pulporo/asgi.py
+++ pulporo/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulporoA1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulporo.settings')
 
 application = get_asgi_application()
