0c4f6baec7c53a6694c291a958226e62a961e6ca
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-09 12:29:24 +0200

    Refactor: Changed name from init_datatable.js to DataTables.js to make it clear that this file holds all code related to Data Tables

---
+++ static/js/DataTables.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let table = new DataTable('#TableIO', {
+        paging: true,
+        order: false,
+        searching: true,
+        colReorder: true,
+        autoWidth: false,
+        fixedHeader: true,
+    });
+    setCalendar();
+});
