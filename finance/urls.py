f68ced137c3707c915b5f1b4a848f40ed1b69b7b
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-11 10:38:08 +0200

    Improvement: Added new links to unified CRUD operations

--- finance/urls.py
+++ finance/urls.py
@@ -13,10 +13,21 @@ urlpatterns = [
     path('reminders/', reminders, name='reminders'),
     path('media/', media, name='media'),
 
-    # Actions
-    path('create/', CreateIO.as_view(), name='create-io'),
-    path('update/<int:pk>/', DetailIO.as_view(), name='detail-io'),
-    path('delete/<int:pk>', DeleteIO.as_view(), name='delete-io')
+    # Actions on OneIO
+    path('create/oneio/<int:is_outcome>/', CreateIO.as_view(), name='create-io'),
+    path('update/oneio/<int:pk>/', DetailIO.as_view(), name='detail-io'),
+    path('delete/oneio/<int:pk>/', DeleteIO.as_view(), name='delete-io'),
+
+    # Actions on Manager
+    path('create/manager/<int:is_outcome>/', CreateIO.as_view(), name='create-manager'),
+    path('update/manager/<int:pk>/', DetailIO.as_view(), name='detail-manager'),
+    path('delete/manager/<int:pk>/', DeleteIO.as_view(), name='delete-manager'),
+
+    # Actions on Investment
+    path('create/investment/', CreateIO.as_view(), name='create-investment'),
+    path('update/investment/<int:pk>/', DetailIO.as_view(), name='detail-investment'),
+    path('delete/investment/<int:pk>/', DeleteIO.as_view(), name='delete-investment')
+
 ]
 
 
