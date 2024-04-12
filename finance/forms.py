4bd73fdce694f71d08d0285899be7c05fe0226f9
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-12 10:13:10 +0200

    Refactor: - Now form fields are easy to edit - Create BaseIOForm to reduce code duplication

--- finance/forms.py
+++ finance/forms.py
@@ -1,38 +1,90 @@
-from django.forms import ModelForm, ValidationError, DateInput, BooleanField
-from django.contrib.auth.models import User
-from .models import OneIO
+from datetime import date
+from collections import OrderedDict
+from django import forms
+from .models import OneIO, ManagerIO
 
 
-class UpdateIOForm(ModelForm):
+class BaseIOForm(forms.ModelForm):
     class Meta:
+        fields = [
+            'title',
+            'value',
+            'notes',
+        ]
+    title = forms.CharField(
+        label=False,
+        widget=forms.TextInput(attrs={
+            'class': 'add-form-title',
+            'placeholder': 'Title',
+        })
+    )
+    value = forms.DecimalField(
+        min_value=0,
+        max_digits=17,
+        decimal_places=2,
+        label=False,
+        widget=forms.NumberInput(attrs={
+            'class': 'add-form-value',
+            'placeholder': 100.01
+        })
+    )
+    notes = forms.CharField(
+        label=False,
+        required=False,
+        widget=forms.Textarea({
+            'class': 'add-form-notes',
+            'placeholder': 'Place your notes here',
+        })
+    )
+
+
+class ManagerIOForm(BaseIOForm):
+    class Meta(BaseIOForm.Meta):
+        model = ManagerIO
+        fields = BaseIOForm.Meta.fields + [
+            'active',
+            'interval_start',
+            'interval_end',
+            'interval',
+        ]
+
+
+class OneIOForm(BaseIOForm):
+    class Meta(BaseIOForm.Meta):
+        fields = BaseIOForm.Meta.fields + ['is_outcome', 'date', 'prediction']
         model = OneIO
-        fields = ['title', 'value', 'is_outcome', 'date', 'prediction', 'cash_tag_id', 'company_id', 'notes']
-        is_outcome = BooleanField()
-        date = DateInput()
-        widgets = {
-            'date': DateInput(attrs={'type': 'date'})
-        }
+
+    date = forms.DateField(
+        label=False,
+        initial=date.today(),
+        widget=forms.DateInput(attrs={
+            'class': 'add-form-date',
+            'type': 'date',
+        })
+    )
+
+    prediction = forms.BooleanField(
+        label=False,
+        initial=True,
+        help_text='Prediction',
+        widget=forms.CheckboxInput(attrs={
+            'class': 'add-form-prediction'
+        })
+    )
+
+    is_outcome = forms.BooleanField(
+        label=False,
+        initial=True,
+        help_text='Outcome',
+        widget=forms.CheckboxInput(attrs={
+            'class': 'add-form-prediction'
+        })
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['cash_tag_id'].required = False
-        self.fields['company_id'].required = False
-        self.fields['notes'].required = False
-        self.fields['is_outcome'].required = False
-        self.fields['is_outcome'].initial = True
-        self.fields['is_outcome'].label = 'Outcome'
-
-    def clean_value(self):
-        value = self.cleaned_data.get('value')
-        if value < 0:
-            raise ValidationError('Value mast be equal or greater than 0')
-        return value
-
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-        if not title and len(title) == 0 or len(title) == 51:
-            raise ValidationError('Title has to be between 1 and 50 charts')
-        return title
-
+        # Reorder fields
+        field_order = ['title', 'is_outcome', 'value', 'date', 'prediction', 'notes']
+        self.fields = OrderedDict((key, self.fields[key]) for key in field_order)
 
 
