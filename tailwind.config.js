c559fde7a70d245fdc8bfacec4b25944650660cd
Simon Piechutowski <szymonpiechutowski@gmail.com>
2024-04-15 14:18:28 +0200

    Made base.css based on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     extend: {
       colors: {
         'dark-grey': '#201F1F',
+        'middle-grey': "#9D9C9C",
         'light-grey': '#D9D9D9',
+        'main-grey': 'rgba(78, 76, 76, 0.75)',
         'purple': '#BAB0F8',
+        'popup-bg': 'rgba(0, 0, 0, 0.5)',
       }
     },
   },
