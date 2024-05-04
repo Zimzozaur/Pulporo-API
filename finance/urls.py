2ac35ec95360fa0f218331961716210db49c5180
bmo <szymonpiechutowski@gmail.com>
2024-05-04 21:42:38 +0200

    Removed all fronted related code and urls, added rest framework.

--- finance/urls.py
+++ finance/urls.py
@@ -6,33 +6,9 @@ urlpatterns = [
     # Base
     path('', views.load_base, name='base'),
 
-    # Views
-    path('dashboard/', views.dashboard, name='dashboard'),
-
-    path('ledger/', views.ListIOs.as_view(), name='ledger'),
-    path('ledger/update-dom/', views.ListIOs.as_view(), name='ledger-update'),
-
-    path('recurring/', views.recurring, name='recurring'),
-    path('investments/', views.investments, name='investments'),
-    path('liabilities/', views.liabilities, name='liabilities'),
-    path('reminders/', views.reminders, name='reminders'),
-    path('media/', views.media, name='media'),
-
     # Actions on OneIO
     path('get/oneio/<int:is_outcome>/', views.CreateIO.as_view(), name='get-OneIO-form'),
     path('post/oneio/<int:is_outcome>/', views.CreateIO.as_view(), name='post-OneIO-form'),
-    path('update/oneio/<int:pk>/', views.DetailIO.as_view(), name='detail-io'),
-    path('delete/oneio/<int:pk>/', views.DeleteIO.as_view(), name='delete-io'),
-
-    # Actions on Manager
-    path('create/manager/<int:is_outcome>/', views.CreateIO.as_view(), name='create-manager'),
-    path('update/manager/<int:pk>/', views.DetailIO.as_view(), name='detail-manager'),
-    path('delete/manager/<int:pk>/', views.DeleteIO.as_view(), name='delete-manager'),
-
-    # Actions on Investment
-    path('create/investment/', views.CreateIO.as_view(), name='create-investment'),
-    path('update/investment/<int:pk>/', views.DetailIO.as_view(), name='detail-investment'),
-    path('delete/investment/<int:pk>/', views.DeleteIO.as_view(), name='delete-investment')
 
 ]
 
--- pulporo/settings.py
+++ pulporo/settings.py
@@ -38,20 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
-    "django_htmx",
+    # 'debug_toolbar',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'pulporo.urls'
