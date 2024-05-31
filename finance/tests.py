eec406d375ccb17e635256c4d82f97e67421fad6
bmo <szymonpiechutowski@gmail.com>
2024-05-31 10:07:05 +0200

    Created test for inflow view

--- finance/tests.py
+++ finance/tests.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from rest_framework.test import APIClient
 from rest_framework.response import Response
 
-from .models import Outflow
+from .models import Outflow, Inflow
 
 
 @pytest.mark.django_db
@@ -52,3 +52,41 @@ class TestOneOffOutflowsView:
         assert response.data == []
 
 
+@pytest.mark.django_db
+class TestOneInflowsView:
+    url: str = reverse('inflows')
+    example_fixture: dict = {
+        'title': 'Mario in DB',
+        'value': '123.45',
+        'date': datetime.now().strftime('%Y-%m-%d'),
+        'notes': ''
+    }
+    api_client: APIClient = APIClient()
+
+    def test_get_request_empty_DB(self):
+        response: Response = self.api_client.get(self.url)
+        assert response.status_code == 200
+        assert response.data == []
+
+    def test_get_request_one_record_DB(self):
+        Inflow.objects.create(**self.example_fixture)
+        response: Response = self.api_client.get(self.url)
+        assert response.data == [self.example_fixture]
+
+    def test_get_request_one_record_DB_current_date(self):
+        Inflow.objects.create(**self.example_fixture)
+        params = {
+            'year': timezone.now().year,
+            'month': timezone.now().month
+        }
+        response: Response = self.api_client.get(self.url, params)
+        assert response.data == [self.example_fixture]
+
+    def test_get_request_one_record_DB_not_current(self):
+        Inflow.objects.create(**self.example_fixture)
+        params = {
+            'year': timezone.now().year,
+            'month': 1
+        }
+        response: Response = self.api_client.get(self.url, params)
+        assert response.data == []
