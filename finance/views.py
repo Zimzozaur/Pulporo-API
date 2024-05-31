8aac6dba563d330cb4edbbd4a30d4df0915d8fb0
bmo <szymonpiechutowski@gmail.com>
2024-05-31 10:03:13 +0200

    Created parent class for inflows and outflow view

--- finance/views.py
+++ finance/views.py
@@ -1,18 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import (
-    ListAPIView
+    ListCreateAPIView, CreateAPIView
 )
 
-from django.http import HttpResponse
-from django.template.loader import render_to_string
-from django.shortcuts import render, redirect
-from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.decorators.http import require_GET
 from django.utils import timezone
-from django.urls import reverse_lazy
-from django.db.models import Sum
 
 from .models import Outflow, Inflow
 from .serializers import (
@@ -33,32 +25,35 @@ class ImagesURLs(APIView):
         return Response(self.JSON)
 
 
-class ListInflows(ListAPIView):
-    queryset = Inflow.objects.all()
+class FlowsBaseView(ListCreateAPIView):
+    """Base class for Inflows and Outflows to return list of them"""
+    model = None
+    serializer_class = None
+
+    def get_queryset(self):
+        if self.model is None:
+            raise NotImplementedError("model attribute must be set for the view.")
+        if self.serializer_class is None:
+            raise NotImplementedError("serializer_class attribute must be set for the view.")
+
+        queryset = self.model.objects.all()
+
+        year = self.request.query_params.get('year', timezone.now().year)
+        month = self.request.query_params.get('month', timezone.now().month)
+
+        res = queryset.filter(date__year=year, date__month=month)
+
+        return res
+
+
+class OneOffInflowsView(FlowsBaseView):
+    """Return list of Inflows"""
+    model = Inflow
     serializer_class = InflowSerializer
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
 
-        year = self.request.query_params.get('year', timezone.now().year)
-        month = self.request.query_params.get('month', timezone.now().month)
-
-        return queryset.filter(date__year=year, date__month=month)
-
-
-
-class ListOutflows(ListAPIView):
-    queryset = Outflow.objects.all()
+class OneOffOutflowsView(FlowsBaseView, CreateAPIView):
+    """Return list of Outflows"""
+    model = Outflow
     serializer_class = OutflowSerializer
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        year = self.request.query_params.get('year', timezone.now().year)
-        month = self.request.query_params.get('month', timezone.now().month)
-
-        return queryset.filter(date__year=year, date__month=month)
-
-
-
 
